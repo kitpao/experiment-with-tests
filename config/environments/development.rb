a3ede01fe76001df2d5c2e92204ec6cd3c69d54c
kitpao <kitpao91@gmail.com>
2020-06-10 18:28:57 +0000

    modify view and add config for server in gitpod

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+config.hosts << "3000-de7043ac-af4e-4d96-8f54-f649005588ea.ws-us02.gitpod.io"
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
